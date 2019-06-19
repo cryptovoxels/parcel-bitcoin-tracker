4a800780189c88c88fc99e3fc1e710285b77ae7f
Ben Nolan <bnolan@gmail.com>
2019-06-19 12:40:24 +1200

    Add index

---
+++ index.js
@@ -0,0 +1,26 @@
+const { Parcel } = require('cryptovoxels')
+const fetch = require('node-fetch')
+
+const p = new Parcel(52)
+
+const API_URL = 'https://api.coindesk.com/v1/bpi/currentprice.json'
+
+p.fetch()
+  .then(() => {
+    setInterval(() => {
+      fetch(API_URL)
+        .then(r => r.json())
+        .then(r => {
+          let price = parseFloat(r.bpi.USD.rate).toFixed(0)
+          let sign = p.getFeatureById('bitcoin-price')
+
+          sign.set({
+            text: `$${price}`,
+            fontSize: 50
+          })
+        })
+    }, 5000)
+
+    let port = p.listen()
+    console.log(`Listening on port ${port}`)
+  })
